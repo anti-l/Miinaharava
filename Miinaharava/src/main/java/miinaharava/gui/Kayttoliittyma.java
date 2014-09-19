f78b6aa5129e6ba76b11f61fdaf9fa717c883b83
anti-l <git@lun.fi>
2014-09-19 19:17:10 +0300

    Työtä käyttöliittymän ja tyhjien ruutujen paljastusmetodien kanssa.

--- Miinaharava/src/main/java/miinaharava/gui/Kayttoliittyma.java
+++ Miinaharava/src/main/java/miinaharava/gui/Kayttoliittyma.java
@@ -24,15 +24,15 @@ public class Kayttoliittyma implements Runnable {
     
     public Kayttoliittyma(Sovelluslogiikka sovelluslogiikka) {
         this.sovelluslogiikka = sovelluslogiikka;
-        ruudukko = sovelluslogiikka.getRuudukko();
+        this.ruudukko = sovelluslogiikka.getRuudukko();
     }
     
     @Override
     public void run() {
         frame = new JFrame("Miinaharava");
         // Näin voidaan muuttaa ikkunan koko siten, että yksittäisen napin koko on aina sama
-        // frame.setPreferredSize(new Dimension(ruudukko.getLeveys()*30, ruudukko.getLeveys()*30+50));
-        frame.setPreferredSize(new Dimension(400, 450));
+         frame.setPreferredSize(new Dimension(ruudukko.getLeveys()*40, ruudukko.getLeveys()*40+50));
+        //frame.setPreferredSize(new Dimension(400, 450));
         frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         luoKomponentit(frame.getContentPane());
         frame.pack();
@@ -53,21 +53,23 @@ public class Kayttoliittyma implements Runnable {
             vaikeustaso = "(vaikea)";
         }
         
-        container.add(new JLabel("Miinaharava #JavaLabra2014 " + vaikeustaso), BorderLayout.NORTH);
+        container.add(new JLabel("Miinaharava #JavaLabra2014 - " + vaikeustaso), BorderLayout.NORTH);
         JPanel nappiruudukko = luoNapit(ruudukko.getLeveys(), ruudukko.getKorkeus());
         container.add(nappiruudukko);
     }
     
     public JPanel luoNapit(int x, int y) {
         JPanel miinanapit = new JPanel(new GridLayout(x, y));
-        napisto = new RuutuNappi[x][y];
+        this.napisto = new RuutuNappi[x][y];
         for (int i = 0; i < x; i++) {
             for (int j = 0; j < y; j++) {
-                napisto[i][j] = new RuutuNappi(ruudukko, i, j);
-                napisto[i][j].addActionListener(new NapinKuuntelija(sovelluslogiikka, napisto[i][j], i, j));
-                miinanapit.add(napisto[i][j]);
+                this.napisto[i][j] = new RuutuNappi(ruudukko, i, j);
+                this.napisto[i][j].addActionListener(new NapinKuuntelija(sovelluslogiikka, napisto[i][j], i, j));
+                miinanapit.add(this.napisto[i][j]);
             }
         }
+        //System.out.println("Napisto valmis");
+        //System.out.println("napisto: " + napisto.length + " napisto[0]:" + napisto[0].length);
         return miinanapit;
     }
     
@@ -78,6 +80,8 @@ public class Kayttoliittyma implements Runnable {
     }
     
     public void painaNappiAlas(int x, int y) {
+//        System.out.println("napisto:" + this.napisto);
+//        System.out.println("napisto:" + this.napisto.length);
         //napisto.length aiheuttaa NullPointerExceptionin
         /*
             if (!(x < 0 || x > napisto.length)) {
--- Miinaharava/src/main/java/miinaharava/sovelluslogiikka/Sovelluslogiikka.java
+++ Miinaharava/src/main/java/miinaharava/sovelluslogiikka/Sovelluslogiikka.java
@@ -27,9 +27,17 @@ public class Sovelluslogiikka {
         this.ruudukko = new Ruudukko(leveys, korkeus, miinoja);
     }
 
+    public void setKayttoliittyma(Kayttoliittyma kayttis) {
+        this.kayttoliittyma = kayttis;
+    }
+
+    public Ruudukko getRuudukko() {
+        return this.ruudukko;
+    }
+
     public void tarkistaRuutu(int x, int y, RuutuNappi nappi) {
         Ruutu ruutu = this.ruudukko.getRuutu(x, y);
-        ruutu.katsoRuutu();
+//        ruutu.katsoRuutu();
         if (ruutu.onkoMiinaa()) {
             nappi.setBackground(Color.RED);
 //            nappi.setText("@");
@@ -48,31 +56,26 @@ public class Sovelluslogiikka {
         nappi.setEnabled(false);
     }
 
-    public Ruudukko getRuudukko() {
-        return this.ruudukko;
-    }
-
     public void paljastaTyhjat(int x, int y) {
-        if (!(ruudukko.getRuutu(x, y).getKatsottu())) {
+        if (ruudukko.getRuutu(x, y).getKatsottu() == false) {
             for (int i = -1; i <= 1; i++) {
                 for (int j = -1; j <= 1; j++) {
-                    if (i == x && j == y) {
+                    if (i == x && j == y) {                                             // Ruutu, mitä pelaaja itse painoi -> jatketaan
                         continue;
-                    } else if (x + i < 0 || x + i >= ruudukko.getLeveys()) {
+                    } else if (x + i < 0 || x + i >= ruudukko.getLeveys()) {            // Ruutu, joka menee vaakasuunnassa ruudukon ulkopuolelle -> jatketaan
                         continue;
-                    } else if (y + j < 0 || y + j >= ruudukko.getKorkeus()) {
+                    } else if (y + j < 0 || y + j >= ruudukko.getKorkeus()) {           // Ruutu, joka menee pystysuunnassa ruudukon ulkopuolelle -> jatketaan
                         continue;
-                    } else if (ruudukko.getRuutu((x + i), (y + j)).onkoTyhja() && ruudukko.getRuutu((x + i), (y + j)).getKatsottu()) {
-                        ruudukko.getRuutu((x + i), (y + j)).katsoRuutu();
-//                        kayttoliittyma.painaNappiAlas(x, y);
-                        paljastaTyhjat(x + i, y + j);
+                    // Katsotun ruudun kaikki 8 naapuria +/- 1 pysty- ja vaakasuuntaan:
+                    // Jos ruutu on tyhjä eikä sitä ole tarkistettu vielä
+                    } else if (ruudukko.getRuutu((x + i), (y + j)).onkoTyhja() && ruudukko.getRuutu((x + i), (y + j)).getKatsottu() == false) {
+                        ruudukko.getRuutu((x + i), (y + j)).katsoRuutu();               // Määritetään Ruutu katsotuksi
+                        kayttoliittyma.painaNappiAlas(x, y);                            // Painetaan käyttöliittymän nappi alas
+                        paljastaTyhjat(x + i, y + j);                                   // Kutsutaan rekursiivisesti viereisiä ruutuja
                     }
                 }
             }
         }
     }
     
-    public void setKayttoliittyma(Kayttoliittyma kayttis) {
-        this.kayttoliittyma = kayttis;
-    }
 }
